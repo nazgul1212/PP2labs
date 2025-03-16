6196f58a141374e23a44f6e72a2d0b0eb66704e2
nazgul1212 <n_khurshitova@kbtu.kz>
2025-03-16 21:35:50 +0500

    .

--- lab7/mickey.py
+++ lab7/mickey.py
@@ -1,53 +1,75 @@
 import pygame
-import time
-import math
+import datetime
+
 
 pygame.init()
-
-size = width, height = 1000, 1000
-screen = pygame.display.set_mode(size)
+W, H = 600, 400
+sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Mickey Clock")
 
-white = (255, 255, 255)
-black = (0, 0, 0)
+time_font = pygame.font.Font('/Users/nazekkhurshitova/Desktop/pp2labs/lab7/font_user.ttf', 24)
 
-minute_hand_img = pygame.image.load("/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/min_hand.png").convert_alpha()
-second_hand_img = pygame.image.load("/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/sec_hand.png").convert_alpha()
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (239, 228, 176)
 
-background_img = pygame.image.load("/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/clock.png").convert()
+def system_time(now):
+    global time_font
+    
+    min = now.strftime('%M')
+    sec = now.strftime('%S')
+    
+    sc_text = time_font.render(f"{min}:{sec}", 1, RED, YELLOW)
+    pos = sc_text.get_rect()
+
+    pos.x, pos.y = 500, 350
+    sc.blit(sc_text, pos)
+
+
+
+clock_sc = pygame.image.load('/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/clock.png').convert_alpha()
+leftarm_sc = pygame.image.load('/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/sec_hand.png').convert_alpha()
+rightarm_sc = pygame.image.load('/Users/nazekkhurshitova/Desktop/pp2labs/lab7/mickey img/min_hand.png').convert_alpha()
+
+clock_sc = pygame.transform.scale(clock_sc, (W, H))
+leftarm_sc = pygame.transform.scale(leftarm_sc, (leftarm_sc.get_width()//2.0, leftarm_sc.get_height()//2.0))
+rightarm_sc = pygame.transform.scale(rightarm_sc, (rightarm_sc.get_width()//2.0, rightarm_sc.get_height()//2.0))
+
+angle = 0
 
 clock = pygame.time.Clock()
-font = pygame.font.SysFont(None, 48)
 
-while True:
-    current_time = time.localtime()
-    seconds = current_time.tm_sec
-    minutes = current_time.tm_min
-    
-    minute_angle = 90 - (minutes / 60) * 360
-    second_angle = 90 - (seconds / 60) * 360
-    
-    minute_hand = pygame.transform.rotate(minute_hand_img, minute_angle)
-    second_hand = pygame.transform.rotate(second_hand_img, second_angle)
-    
-    screen.blit(background_img, background_img.get_rect(center=(width//2, height//2)))
-    pygame.draw.circle(screen, black, (width//2, height//2), 150, 5)
-    pygame.draw.circle(screen, black, (width//2, height//2), 5)
-    
-    minute_hand_rect = minute_hand.get_rect(center=(width//2, height//2))
-    second_hand_rect = second_hand.get_rect(center=(width//2, height//2))
-    screen.blit(minute_hand, minute_hand_rect)
-    screen.blit(second_hand, second_hand_rect)
-    
-    text = font.render(time.strftime("%H:%M:%S", time.localtime()), True, black)
-    text_rect = text.get_rect(center=(width//2, height-50))
-    screen.blit(text, text_rect)
-    
-    pygame.display.flip()
-    
+leftarm_rect = leftarm_sc.get_rect(center=(W//2, H//2))
+rightarm_rect = rightarm_sc.get_rect(center=(W//2, H//2))
+
+sc.blit(leftarm_sc, leftarm_rect)
+sc.blit(rightarm_sc, rightarm_rect)
+
+
+while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             exit()
+
+    now = datetime.datetime.now()
+    sc.fill((255, 255, 255))
+    sc.blit(clock_sc, (0, 0))
+
+    angle = -(now.second+1)*6
+    angle2 = -(now.minute)*6
+
+    leftarm_sc_rotated = pygame.transform.rotate(leftarm_sc, angle)
+    leftarm_rect_rotated = leftarm_sc_rotated.get_rect(center=leftarm_rect.center)
+
+    rightarm_sc_rotated = pygame.transform.rotate(rightarm_sc, angle2-54)
+    rightarm_rect_rotated = rightarm_sc_rotated.get_rect(center = rightarm_rect.center)
+
     
-    clock.tick(60)
+    sc.blit(leftarm_sc_rotated, leftarm_rect_rotated)
+    sc.blit(rightarm_sc_rotated, rightarm_rect_rotated)
+
+    system_time(now)
+    
+    pygame.display.update()
+
+    clock.tick(1)
